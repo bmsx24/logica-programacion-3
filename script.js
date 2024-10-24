2b1a089853c56a44a4cb826e918f51f10e04d367
bmsx24 <bmsx.user@gmail.com>
2024-10-23 20:50:32 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const numero = document.getElementById("numero");
 const resultado = document.getElementById("resultado");
 
 function operar() {
-    const n = parseInt(numero.value); // Leer el valor dentro de la función
+    const n = parseInt(numero.value);
     if (isNaN(n) || n < 0) {
         resultado.textContent = "Error: La entrada debe ser un número entero no negativo.";
     } else {
         function factorial(n) {
-            return (n > 1) ? n * factorial(n - 1) : 1; // Cambiar la condición a n > 1
+            return (n > 1) ? n * factorial(n - 1) : 1; 
         }
         resultado.textContent = 'El factorial de ' + n + ' es: ' + factorial(n);
     }
